7462095d61052b4d1fbcb155347347406af5980a
Mike Heavers <mike.heavers@instrument.com>
2018-05-16 14:54:31 -0700

    connect progress

--- js/connect.js
+++ js/connect.js
@@ -1,71 +1,136 @@
 var lineWidth = 5;
+var curMarkIndex = 0;
+var marksArr = ['line-house-rev.svg', 'line-investing.svg'];
+var markPosArr = [
+    { x: .2, y: .25 },
+    { x: .6, y: .25 }
+]
+var drawingsArr = [];
+var markWidth = 0;
+
+var firstCoord = { x: 0, y: 0 }
+var lastCoord = { x: 0, y: 0 }
 
 $(function () {
-	var two = new Two({
-		type: Two.Types['svg'],
-		fullscreen: true
-	}).appendTo(document.body);
-	$.get('../images/line-house.svg', function(doc) {
-		var fresh = two.interpret($(doc).find('svg')[0]);
-		fresh.subdivide();
-		fresh.noFill();
-		var t = 0;
-		var startOver,
-			movingmouse = false;
-		var clearT = function() {
-			t = 0;
-			setEnding(fresh, 0);
-			startOver = _.after(60, clearT);
-		};
-		fresh.center().translation.set(two.width / 2, two.height / 2);
-		fresh.distances = calculateDistances(fresh);
-		fresh.total = 0;
-		fresh.stroke = 'white';
-		fresh.linewidth = lineWidth;
-		_.each(fresh.distances, function(d) {
-			fresh.total += d;
-		});
-
-		clearT();
-
-		resize();
-		two
-			.bind('resize', resize)
-            .bind('update', function () {
-                fresh.lineWidth = getRandomInt(3, 50);
-                //lineWidth = getRandomInt(3, 5);
-                //console.log(lineWidth);
-				if (t < 0.9999) {
-					t += 0.00625;
-				} else {
-					startOver();
-				}
-
-				setEnding(fresh, t);
-			})
-			.play();
-
-		function resize() {
-			fresh.translation.set(two.width / 2, two.height / 2);
-		}
-
-		function setEnding(group, t) {
-			var i = 0;
-			var traversed = t * group.total;
-			var current = 0;
-
-			_.each(group.children, function(child) {
-				var distance = group.distances[i];
-				var min = current;
-				var max = current + distance;
-				var pct = cmap(traversed, min, max, 0, 1);
-				child.ending = pct;
-				current = max;
-				i++;
-			});
-		}
-	});
+    drawMark();
 });
+
+function drawConnector() {
+    var two = new Two({
+        type: Two.Types['svg'],
+        fullscreen: true,
+        autostart: true
+    }).appendTo(document.body);
+    var line = new Two.Vector(), randomness = 0;
+    line = two.makeCurve([new Two.Vector(lastCoord.x, lastCoord.y), new Two.Vector(firstCoord.x, firstCoord.y)], true);
+    line.noFill().stroke = 'white';
+    line.linewidth = lineWidth;
+    line.cap = 'round';
+
+}
+
+function drawMark() {
+    $.get(`../images/${marksArr[curMarkIndex]}`, function (doc) {
+        var two = new Two({
+            type: Two.Types['svg'],
+            fullscreen: true
+        }).appendTo(document.body);
+        markWidth = two.width / marksArr.length;
+        var storyMark = two.interpret($(doc).find('svg')[0]);
+        storyMark.subdivide();
+        storyMark.noFill();
+        var t = 0;
+        var startOver,
+            movingmouse = false;
+        var clearT = function () {
+            t = 0;
+            setEnding(storyMark, 0);
+            startOver = _.after(60, clearT);
+        };
+        //storyMark.center().translation.set(curMarkIndex*markWidth, two.height / 2);
+        storyMark.distances = calculateDistances(storyMark);
+        storyMark.total = 0;
+        storyMark.stroke = 'white';
+        storyMark.linewidth = lineWidth;
+        storyMark.cap = 'round';
+        _.each(storyMark.distances, function (d) {
+            storyMark.total += d;
+        });
+
+        clearT();
+        resize();
+        two
+            .bind('resize', resize)
+            .bind('update', function () {
+                if (t < 0.9999) {
+                    if (t === 0) {
+
+                        if (curMarkIndex > 0) {
+                            var firstChild = storyMark.children[0];
+                            var firstVertex = firstChild.vertices[0];
+                            firstCoord.x = storyMark.translation.x + firstChild.translation.x + firstVertex.x;
+                            firstCoord.y = storyMark.translation.y + firstChild.translation.y + firstVertex.y;
+                            drawConnector();
+                        }
+
+                    }
+                    t += 0.00625;
+                    setEnding(storyMark, t);
+                } else {
+                    if (curMarkIndex < marksArr.length - 1) {
+                        var lastChildIndex = storyMark.children.length - 1;
+                        var lastChild = storyMark.children[lastChildIndex];
+                        var lastVertexIndex = lastChild.vertices.length - 1;
+                        var lastVertex = lastChild.vertices[lastVertexIndex];
+                        lastCoord.x = storyMark.translation.x + lastChild.translation.x + lastVertex.x;
+
+                        lastCoord.y = storyMark.translation.y + lastChild.translation.y + lastVertex.y;
+                        //console.log(storyMark,lastChild,lastVertex);
+                    }
+
+                    two.pause();
+                    if (curMarkIndex < marksArr.length - 1) {
+                        curMarkIndex++;
+                        drawMark();
+                    } else {
+                        curMarkIndex = 0;
+                        //startOver
+                    }
+                }
+            })
+            .play();
+
+        function resize() {
+            //var markWidth = storyMark.getBoundingClientRect().width;
+            //var markHeight = storyMark.getBoundingClientRect.height;
+            //console.log(storyMark.getBoundingClientRect());
+            //var markPos = (two.width * ((curMarkIndex + 1) / marksArr.length))-markWidth;
+            //storyMark.translation.set(markPos, two.height / 2);
+            storyMark.translation.set(markPosArr[curMarkIndex].x * two.width, markPosArr[curMarkIndex].y * two.height);
+        }
+
+        function setEnding(group, t, last = false) {
+            var i = 0;
+            var traversed = t * group.total;
+            var current = 0;
+
+            _.each(group.children, function (child, j) {
+                var distance = group.distances[i];
+                var min = current;
+                var max = current + distance;
+                var pct = cmap(traversed, min, max, 0, 1);
+                child.ending = pct;
+                current = max;
+                i++;
+                if (last) {
+                    if (j === group.children.length-1) {
+                        console.log(child);
+                    }
+                }
+            });
+        }
+    });
+}
 
 function calculateDistances(group) {
 	return _.map(group.children, function(child) {
@@ -91,8 +156,4 @@ function map(v, i1, i2, o1, o2) {
 
 function cmap(v, i1, i2, o1, o2) {
 	return clamp(map(v, i1, i2, o1, o2), o1, o2);
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
